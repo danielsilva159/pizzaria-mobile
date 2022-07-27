0ad623a9e8f6f3326754ec8d89488a2f7dc5b067
Daniel Ferreira <silva_120000@yahoo.com.br>
2022-07-27 07:33:22 -0300

    finalizando listagem de pedido

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, TextInput, Modal} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity, TextInput, Modal, FlatList} from 'react-native'
 import { useRoute, RouteProp, useNavigation} from '@react-navigation/native'
 import {Feather} from '@expo/vector-icons'
 import { api } from '../../services/api';
 import { ModalPicker } from '../../components/ModalPicker';
+import { ListItem } from '../../components/ListItem';
 
 type RouteDetailParams = {
     Order:{
@@ -92,13 +93,46 @@ export default function Order(){
         setProductSelected(item);
     }
 
+    async function handleAdd() {
+        const response = await api.post('/order/add', {
+            order_id: route.params.order_id,
+            product_id: productSelected?.id,
+            amount: Number(amount)
+        })
+        let data = {
+            id: response.data.id,
+            product_id: productSelected?.id as string,
+            name: productSelected?.name as string,
+            amount: amount
+        }
+        setItems(oldArray => [...oldArray, data])
+    }
+
+    async function handleDeleteItem(item_id: string){
+        await api.delete('/order/remove', {
+            params:{
+                item_id: item_id
+            }
+        })
+
+        let removeItem = items.filter(item => {
+            return( item.id !== item_id)
+        })
+
+        setItems(removeItem);
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.title}>Mesa {route.params.number}</Text>
-                <TouchableOpacity onPress={handleCloseOrder}>
-                    <Feather name="trash-2" size={28} color="#FF3F4b" />
-                </TouchableOpacity>
+                {
+                    items.length === 0 && (
+                        <TouchableOpacity onPress={handleCloseOrder}>
+                            <Feather name="trash-2" size={28} color="#FF3F4b" />
+                        </TouchableOpacity>
+                    )
+                }
             </View>
             {
                 category.length !== 0 && (
@@ -129,7 +163,7 @@ export default function Order(){
                 />
             </View>
             <View style={styles.actions}>
-                <TouchableOpacity style={styles.buttonAdd}>
+                <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
                     <Text style={styles.buttonText}>+</Text>
                 </TouchableOpacity>
                 <TouchableOpacity 
@@ -139,6 +173,14 @@ export default function Order(){
                 </TouchableOpacity>
 
             </View>
+
+            <FlatList 
+                showsVerticalScrollIndicator={false}
+                style={{flex: 1, marginTop: 24}}
+                data={items}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) =>  <ListItem data={item} deleteItem={handleDeleteItem} />}
+            />
 
             <Modal transparent={true} visible={modalCategoryVisible} animationType='fade'>
                 <ModalPicker
